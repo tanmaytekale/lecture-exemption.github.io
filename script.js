0ffcad6002fca988c2e321ada4478d6dd11b8528
Tanmay Tekale <75844090+tanmaytekale@users.noreply.github.com>
2026-01-07 19:32:12 +0530

    Update Google Apps Script URL in script.js

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         // REPLACE THIS WITH YOUR GOOGLE APPS SCRIPT WEB APP URL
-        const SCRIPT_URL = 'INSERT_YOUR_GOOGLE_APPS_SCRIPT_URL_HERE';
+        const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxD7lwRC2lUwBM_wMNxH8ygdQxgqPS5QvVfalHPqWTuLcXckpsw8VfwwjMK9Lw1yYx3/exec';
 
         const requestData = {
             personal: {
